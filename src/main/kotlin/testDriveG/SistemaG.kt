18cd3ad8fdfe3f958e5082181c164917679d73ae
Gustavo <gu.losada@hotmail.com>
2021-10-03 15:03:34 -0300

    Criação de ReviewDAO
    ReviewDAO com principais funções prontas e funcionando

--- src/main/kotlin/testDriveG/SistemaG.kt
+++ src/main/kotlin/testDriveG/SistemaG.kt
@@ -1,23 +1,21 @@
 package testDriveG
 
 import models.Films
+import models.Review
 import testDriveG.dao.FilmDAO
+import testDriveG.dao.ReviewDAO
 
 class SistemaG {
     //init?
     fun run(){
-        var filmDAO = FilmDAO()
-        //filmDAO.addOne(Films(0,"Parasite","thriller","Bong Joon-ho","11/07/2019"))
-        //filmDAO.addOne(Films(0,"Parasite","thriller","Bong Joon-ho","11/07/2019"))
-        //println("Info filme parasite:")
-        //println(filmDAO.getOne("Parasite"))
-        //println("Filme com id = 1:")
-        //println(filmDAO.getOne(1))
-        //println("Todos os filmes")
+        val reviewDAO = ReviewDAO()
+
+        reviewDAO.update(Review(5,3,1,"Homem De Ferro S2",25,4.3,"10/3/2021"))
 
         //filmDAO.delete(4)
 
         //filmDAO.update(Films(3,"Parasite","thriller","Bong Joon-ho","11/07/2019"))
+        //filmDAO.addOne(Films(0,"Grown Ups 2","comedy","Dennis Dugan","08/16/2013"))
 /*
         filmDAO.addAll(
             listOf<Films>(
@@ -26,10 +24,9 @@ class SistemaG {
             )
         )
 */
-        //filmDAO.addOne(Films(0,"Grown Ups 2","comedy","Dennis Dugan","08/16/2013"))
-        var films = filmDAO.getAll()
-        for (film in films){
-            println(film)
+        val reviews = reviewDAO.getAll()
+        for (review in reviews){
+            println(review)
         }
     }
 }
---
+++ src/main/kotlin/testDriveG/dao/ReviewDAO.kt
@@ -0,0 +1,174 @@
+package testDriveG.dao
+
+import dao.ConnectionDAO
+import models.Review
+
+class ReviewDAO : GenericDAO {
+
+    //Funcionando
+    //Função para pegaar um review com base no seu id.
+    //Function to get one review from table by id.
+    override fun getOne(id : Int): Any {
+        val connectionDAO = ConnectionDAO()
+        var review : Review? = null        //isso aqui está ruim
+        try {
+            val resultSet = connectionDAO.executeQuery("SELECT * FROM Reviews WHERE id == ${id};")
+            review = Review(
+                resultSet!!.getInt("id"),
+                resultSet.getInt("idUser"),
+                resultSet.getInt("idFilm"),
+                resultSet.getString("review"),
+                resultSet.getInt("likes"),
+                resultSet.getDouble("score"),
+                resultSet.getString("data")
+
+            )
+        }
+        catch(exception:Exception){
+            exception.stackTrace
+        }
+        finally {
+            connectionDAO.close()
+        }
+        return review!!
+    }
+
+    //Funcionando
+    //Função para pegar toda a tabela 'Reviews'.
+    //Function to get all elements from Reviews table.
+    override fun getAll(): List<Any> {
+        val connectionDAO = ConnectionDAO()
+        val review = mutableListOf<Review>()
+        try{
+            val resultSet = connectionDAO.executeQuery("SELECT * FROM Reviews;")
+            while(resultSet?.next()!!){
+                review.add(Review(
+                    resultSet.getInt("id"),
+                    resultSet.getInt("idUser"),
+                    resultSet.getInt("idFilm"),
+                    resultSet.getString("review"),
+                    resultSet.getInt("likes"),
+                    resultSet.getDouble("score"),
+                    resultSet.getString("data"))
+                )
+            }
+        }
+        catch(exception : Exception){
+            exception.stackTrace
+        }
+        finally{
+            connectionDAO.close()
+        }
+        return review
+    }
+
+    //Funcionando
+    //Função que permite adicionar um review na tabela.
+    //Function to add review in table.
+    override fun addOne(obj : Any){
+        val connectionDAO = ConnectionDAO()
+        try{
+            val preparedStatement = connectionDAO.getPreparedStatement("""
+                INSERT INTO Reviews
+                (idUser, idFilm, review, likes, score, data)
+                VALUES (?,?,?,?,?,?)
+            """.trimIndent())
+            val review = obj as Review
+            preparedStatement?.setInt(1,review.idUser)
+            preparedStatement?.setInt(2,review.idFilm)
+            preparedStatement?.setString(3,review.review)
+            preparedStatement?.setInt(4,review.likes)
+            preparedStatement?.setDouble(5,review.score)
+            preparedStatement?.setString(6,review.data)
+            preparedStatement?.executeUpdate()
+        }
+        catch (exception:Exception){
+            exception.stackTrace
+        }
+        finally {
+            connectionDAO.close()
+        }
+    }
+
+    //Funcionando
+    //Função que permite adicinar todos os reviews em uma lista.
+    //Function to add all reviews in list to table.
+    override fun addAll(list : List<Any>){
+        val connectionDAO = ConnectionDAO()
+        try{
+            val preparedStatement = connectionDAO.getPreparedStatement("""
+                INSERT INTO Reviews
+                (idUser, idFilm, review, likes, score, data)
+                VALUES (?,?,?,?,?,?)
+            """.trimIndent())
+            for (obj in list){
+                val review = obj as Review
+                preparedStatement?.setInt(1,review.idUser)
+                preparedStatement?.setInt(2,review.idFilm)
+                preparedStatement?.setString(3,review.review)
+                preparedStatement?.setInt(4,review.likes)
+                preparedStatement?.setDouble(5,review.score)
+                preparedStatement?.setString(6,review.data)
+                preparedStatement?.executeUpdate()
+            }
+        }
+        catch(exception:Exception){
+            exception.stackTrace
+        }
+        finally{
+            connectionDAO.close()
+        }
+    }
+
+    //Funcionando
+    //Função permite deletar um review da tabela pelo seu id.
+    //Function to delete one review from table by the id.
+    override fun delete(id : Int){
+        val connectionDAO = ConnectionDAO()
+        try {
+            val preparedStatement = connectionDAO.getPreparedStatement("""
+                DELETE FROM Reviews
+                WHERE id = ?;
+                """.trimIndent()
+            )
+            preparedStatement?.setInt(1, id)
+            preparedStatement?.executeUpdate()
+        }
+        catch(exception:Exception){
+            exception.stackTrace
+        }
+        finally{
+            connectionDAO.close()
+        }
+    }
+
+    //Funcionando
+    //Função permite atualizar um review da tabela.
+    //Function to update one review from table.
+    override fun update(obj : Any){
+        val connectionDAO = ConnectionDAO()
+        try {
+            val preparedStatement = connectionDAO.getPreparedStatement("""
+                UPDATE Reviews
+                SET idUser = ?, idFilm = ?, review = ?, likes = ?, score = ?, data = ?
+                WHERE id = ?;
+            """.trimIndent())
+            val review = obj as Review
+            preparedStatement?.setInt(1,review.idUser)
+            preparedStatement?.setInt(2,review.idFilm)
+            preparedStatement?.setString(3,review.review)
+            preparedStatement?.setInt(4,review.likes)
+            preparedStatement?.setDouble(5,review.score)
+            preparedStatement?.setString(6,review.data)
+            preparedStatement?.setInt(7,review.id)
+            preparedStatement?.executeUpdate()
+        }
+        catch(exception:Exception){
+            exception.stackTrace
+        }
+        finally{
+            connectionDAO.close()
+        }
+    }
+
+}
