ba5d9bae81d333426b174e8c541ccce250dca6f6
GusLos <gu.losada@hotmail.com>
2021-11-23 08:11:07 -0300

    Limpando pasta de test drive
    Apagando teste drive de classes que já estão sendo usadas.

--- src/main/kotlin/controller/MainController.kt
+++ src/main/kotlin/controller/MainController.kt
@@ -6,15 +6,12 @@ import io.ktor.server.netty.*
 import io.ktor.serialization.*
 import io.ktor.application.*
 import io.ktor.http.*
-import testdrivers.FilmDAOTestDrive
-import testdrivers.ReviewDAOTestDrive
-import testdrivers.UserDAOTestDrive
 import routes.*
 
 class MainController {
     companion object {
         private fun printTestSeparator() { println("\n**__*************________***************________*************__**\n\n")}
-
+/*
         fun testDAOs(select: Int = 1, update: Int = 1, delete: Int = 1) {
             println("\n")
             ReviewDAOTestDrive.testAll(select, update, delete)
@@ -24,7 +21,7 @@ class MainController {
             printTestSeparator()
 
             UserDAOTestDrive.testAll(select, update, delete)
-        }
+        }*/
 
         fun runKtor(){
             embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
